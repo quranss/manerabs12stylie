a4fc755766af5d8a4848701af3677e7c33c4af17
quranis123stylies <42019843+quranss@users.noreply.github.com>
2018-08-02 08:05:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,3 +38,25 @@ voiceChannel.leave();
   }
 
 });
+
+client.on('message', function(message) {
+	const myID = "449506099268419595";
+    let args = message.content.split(" ").slice(1).join(" ");
+    if(message.content.startsWith(prefix + "setname")) {
+		        if(message.author.id !== myID) return;
+            if(!args) return message.reply('اكتب الحالة اللي تريدها.');
+        client.user.setUsername(args);
+        message.channel.send(':white_check_mark: Done!').then(msg => {
+           msg.delete(5000);
+          message.delete(5000);
+        });
+    } else if(message.content.startsWith(prefix + "stream")) {
+		        if(message.author.id !== myID) return;
+            if(!args) return message.reply('اكتب الحالة اللي تريدها.');
+        client.user.setGame(args , 'https://twitch.tv/6xlez1');
+        message.channel.send(':white_check_mark: Done!').then(msg => {
+           msg.delete(5000);
+          message.delete(5000);
+        });
+    }
+});
