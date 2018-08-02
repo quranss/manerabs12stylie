191b6c693b9d12b55e094fbeca26b5cf757b25ac
quranis123stylies <42019843+quranss@users.noreply.github.com>
2018-08-02 10:15:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -60,31 +60,3 @@ client.on('message', function(message) {
         });
     }
 });
-
-const Discord = require('discord.js');
-const client = new Discord.Client({disableEveryone: true})
-const Music = require('discord.js-musicbot-addon');
-client.on('ready', ()=> {
-    console.log('Ready!')
-})
- Music.start(client, {
-    prefix: "!", // البرفكس هنا
-    youtubeKey: 'AIzaSyDIv8O9OwZdoTaghK3_SQmyGcQFzkXMW6E', //هنا ال yt v3 api key
-    global: false, // يكون للسيرفرات ؟ خليه false, يكون queue مشترك خليه true            
-    maxQueueSize: 50, // اقصى حجم لل queue        
-    playCmd: 'play', // امر التشغيل
-    volumeCmd: 'vol',     // امر الصوت
-    thumbnailType: 'high',
-    leaveCmd: 'stop',    // امر المغادرة
-    anyoneCanSkip: true, // تبي الكل يقدر يسوي skip? true > yep, false > nope
-    disableLoop: false, // تبي يكون فيه اعادة؟ , true > yep, false > nope
-    searchCmd: 'search', //امر البحث 
-    requesterName: true, 
-    inlineEmbeds: false,     
-    queueCmd: 'queue', // امر القائمة
-    pauseCmd: 'pause', // امر الايقاف المؤقت
-    resumeCmd: 'resume', // امر الاستمرار
-    skipCmd: 'skip', // امر السكب
-    loopCmd: 'loop', // امر الاعادة
-    enableQueueStat: true,
-  });
