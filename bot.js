ef6b500e4ddc630151604137e3714ee9ddf63b1d
quranis123stylies <42019843+quranss@users.noreply.github.com>
2018-08-02 07:52:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,4 +6,35 @@ client.on('ready', () => {
 });
 
 client.login(process.env.BOT_TOKEN);
+//test
 
+client.on('message', message => {
+const yt = require('ytdl-core');
+  if (message.content.startsWith('#quran')) {
+              if(!message.channel.guild) return message.reply('** This command only for servers **');
+
+    const voiceChannel = message.member.voiceChannel;
+    if (!voiceChannel) {
+      return message.reply(`من فضلك ادخل روم صوتي `);
+    }
+    voiceChannel.join()
+      .then(connnection => {
+        let stream = yt('https://www.youtube.com/watch?v=9-oGnGaI9Ps&t=8009s', {audioonly: true});
+        const dispatcher = connnection.playStream(stream);
+        dispatcher.on('end', () => {
+          voiceChannel.leave();
+        });
+      });
+  }
+  
+  if (message.content.startsWith('#stop')) {
+              if(!message.channel.guild) return message.reply('** This command only for servers **');
+
+    const voiceChannel = message.member.voiceChannel;
+    if (!voiceChannel) {
+      return message.reply(`من فضلك ادخل روم صوتي `);
+    }
+voiceChannel.leave();
+  }
+
+});
