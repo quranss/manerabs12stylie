8026d3f9f742f2d35e0a0edd4b51134fce0d9a84
quranis123stylies <42019843+quranss@users.noreply.github.com>
2018-08-05 09:17:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -278,7 +278,7 @@ client.on('message', msg => {
   command = command.slice(prefix.length);
   let args = msg.content.split(" ").slice(1);
 
-    if(command === "!clear") {
+    if(command === "clear") {
         const emoji = client.emojis.find("name", "wastebasket")
     let textxt = args.slice(0).join("");
     if(msg.member.hasPermission("MANAGE_MESSAGES")) {
